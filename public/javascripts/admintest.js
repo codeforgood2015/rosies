e6ce9b43dfc77077dc97b0051d54a22c2f226e25
Hanna Lee <hannalee995@gmail.com>
2015-01-12 22:25:44 -0500

    minor changes to admin view

--- public/javascripts/admintest.js
+++ public/javascripts/admintest.js
@@ -1,8 +1,7 @@
 //using angular 
-var main = function() {
-	var app = angular.module('admin', []); //create a new app with a name and list of dependencies
+	(function(){var app = angular.module('admin', []); //create a new app with a name and list of dependencies
 	//make a new controller
-	app.controller('adminController', function($scope) {
+	app.controller('AdminController', function($scope) {
 		//to determine when to hide and show different divs
 			//section numbers -- 0:login, 1:actions, 2:viewguests, 3:hours, 4:accounts
 		$scope.currentSection = 0;
@@ -22,13 +21,12 @@ var main = function() {
 		}
 		//to be continued
 
-
 	});
 	//animate the viewing of who is signed up for various dates and times
 	//... to be continued
 
-};
+}());
 
 
 //document ready
-$(document).ready(main);
+//$(document).ready(main);
