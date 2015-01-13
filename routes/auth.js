ea0b1e229c271b7326ab9ac78c1969aa6fe57b85
Shi-Ke Xue <shikexue@mit.edu>
2015-01-13 16:19:13 -0500

    start of authentication flow for guests

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-
 var passport = require('passport');
 var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var LocalStrategy = require('passport-local').Strategy;
+var Guest = require('../models/guest').Guest
 
+// Once we figure our how to get Salesforce working, we should
+// use a module other than passport to authenticate with OAuth2,
+// since we don't really want to serialize a session based on this
 passport.use('oauth', new OAuth2Strategy({
 	authorizationURL: 'https://login.salesforce.com/services/oauth2/authorize',
 	tokenURL: 'https://login.salesforce.com/services/oauth2/token',
@@ -16,8 +20,51 @@ passport.use('oauth', new OAuth2Strategy({
 	console.log('token acquired');
 }));
 
+// until we get Salesforce working, create a local strategy for
+// authenticating users with accounts in our database.
+// Note that we need two authentication flows for admins compared to guests
+passport.use('guest', new LocalStrategy(function(username, password, done) {
+	Guest.findOne({username: username}, function(err, guest) {
+		if (err) {
+			return done(err);
+		} if (!guest) {
+			return done(null, false, {message: 'Incorrect username.'});
+		} if (!guest.validPassword(password)) {
+			return done(null, false, {message: 'Incorrect password.'});
+		}
+		return done(null, guest);
+	});
+}));
+
 router.get('/oauth', passport.authenticate('oauth'));
 
 router.get('/oauth/callback', passport.authenticate('oauth', {successRedirect: '/', failureRedirect: '/'}));
+
+// TODO: set a different failure redirect as needed
+router.put('/guest', passport.authenticate('guest', {successRedirect: '/', failureRedirect: '/', failureFlash: true}));
+
+// once we get Salesforce working, this should be removed as well.
+router.post('/guest', function(req, res) {
+	var data = {
+		username: req.body.username,
+		password: req.body.password
+	};
+	var guest = new Guest(data);
+	guest.save(function(err)) {
+		
+	}
+});
+
+router.get('/guest', function(req, res) {
+	// TODO: render login view
+});
+
+// Clears the session
+router.get('/logout', function(req, res) {
+	// This avoids bugs with Passport's req.logout()
+	req.session.destroy(function(err) {
+		res.redirect('/');
+	});
+});
 
 module.exports = router;
