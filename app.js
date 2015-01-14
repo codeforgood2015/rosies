57b7cb5186fe423101d7688b074431dfdedf738c
treeshuh <tricia.shi@gmail.com>
2015-01-14 16:16:12 -0500

    eek agenda is hard

--- app.js
+++ app.js
@@ -4,24 +4,10 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var http = require('http');
 var Agenda = require('agenda');
+var Rule = require('./models/rules').model;
 var Timeslot = require('./models/timeslot').model;
 var mongoose = require('mongoose');
 var connection_string = 'localhost/rosies';
-
-// ------------------------------------------------------------
-//based off of README.md on https://github.com/rschmukler/agenda
-var agenda = new Agenda({db: {address: 'localhost:27017/agenda-example'}});
-
-
-agenda.define('update timeslots', function(job, done) {
-	var data = job.attrs.data;
- 	done();  
-});
-
-agenda.every('midnight', 'update timeslots', {time: new Date(), capacity: Number, guests: Array, waitlist: Array});
-agenda.start();
-
-// ---------------------------------------------------------------
 
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
@@ -34,6 +20,59 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 // db.once('open', function(callback) {});
 
+// ------------------------------------------------------------
+//based off of README.md on https://github.com/rschmukler/agenda
+/*var agenda = new Agenda({db: {address: 'localhost:27017'}});
+
+
+agenda.define('update timeslots', function(job, done) {
+	console.log("got here 0");
+	var yesterday = new Date(Date.now() - 1000*60*60*24);
+	var tomorrow = new Date(Date.now() + 1000*60*60*24);
+	console.log("got here 1");
+	//delete yesterday's timeslots
+	Timeslot
+	.findAndModify({
+		query: {dayOfWeek: yesterday.toLocaleDateString('en-US', {weekday: 'long'})},
+		update: 
+	});
+	//create tomorrow's timeslots
+	//right now only searches for the weekly timeslots
+	console.log("got here 2");
+	Rule.find({day: 'Friday'}).exec(function(err, rules){
+		console.log(err)
+		if(err || !rules){
+			console.log("uhoh")
+		}
+		else{
+			console.log("looking for rules");
+			for(var i = 0; i <= rules.length; i++){
+				timeslot = new Timeslot({
+					dayOfWeek: tomorrow.toLocaleDateString('en-US', {weekday: 'long'}),
+					date: tomorrow,
+					time: rules[i].time,
+					maxCapacity: rules[i].maxCap,
+					maxWaitlist: rules[i].maxWaitlist,
+					guests: [],
+					waitlist: []
+				})
+			}
+		}
+	})
+	console.log("got here 3");
+	//remove events if they were no repeat
+ 	//not implemented yet
+ 	Timeslot.find().exec(function(err, timeslots){console.log(timeslots)})
+ 	console.log("got here 4");
+ 	done();  
+});
+//cron format: minute, hour, dayOfMonth, monthOfYear, dayOfWeek, Year, * means any
+//currently every day at 12:01AM
+agenda.schedule('* * * * * *', 'update timeslots');
+agenda.start();
+console.log("starting")*/
+
+// ---------------------------------------------------------------
 // routes for the app
 var auth = require('./routes/auth');
 
