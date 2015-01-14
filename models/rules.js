346594e9c9ee95f4853ee4406089019af53d11b9
Hanna Lee <hannalee995@gmail.com>
2015-01-14 14:18:49 -0500

    rules schema

---
+++ models/rules.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId();
+
+var rulesSchema = mongoose.Schema( {
+	maxCap: Number, 
+	maxWaitlist: Number,
+	time: [String],
+	day: Schema.Types.Mixed,
+	repeat: Boolean //true if rule should repeat yearly
+});
+
+module.exports.Rule = mongoose.model('Rule', rulesSchema);
